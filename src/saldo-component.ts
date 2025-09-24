3bdacf2f0b4fd54172752eed3a9b04ec33e4c41e
Henrique Peixoto <henriquepeixoto@MacBook-Pro-de-Henrique.local>
2025-09-24 14:14:54 +0200

    Salvando progresso

---
+++ src/saldo-component.ts
@@ -0,0 +1,14 @@
+let saldo = 3000
+
+const elementoSaldo = document.querySelector('.saldo-valor .valor') as HTMLElement
+
+if (elementoSaldo != null) {
+    elementoSaldo.textContent = formatarMoeda(saldo)
+}
+
+const elementoDataAcesso = document.querySelector('.block-saldo time') as HTMLElement
+
+if (elementoDataAcesso != null) {
+    const dataAcesso: Date = new Date()
+    elementoDataAcesso.textContent = formatarData(dataAcesso, FormatoData.DIA_SEMANA_DIA_MES_ANO)
+}
